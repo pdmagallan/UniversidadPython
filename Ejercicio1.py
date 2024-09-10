3c89585f3982e15382f083171ecbd110f13e85db
Pablo Daniel Magallán <82406744+pdmagallan@users.noreply.github.com>
2024-09-10 15:01:55 -0300

    Create Ejercicio1.py
    Manda a imprimir (mostrar) en consola el siguiente mensaje usando Python:
    
    Bienvenido a tu curso de Universidad Python
    
    Nota: El texto debe ser idéntico al mostrado, respetando acentos, mayúsculas y minúsculas, no agregar espacios, textos o saltos de línea innecesarios, de lo contrario la prueba a ejecutar podría fallar.

---
+++ Ejercicio1.py
@@ -0,0 +1,6 @@
+# Manda a imprimir (mostrar) en consola el siguiente mensaje usando Python:
+# Bienvenido a tu curso de Universidad Python
+#Nota: El texto debe ser idéntico al mostrado, respetando acentos, mayúsculas y minúsculas, no agregar espacios, textos o saltos de línea innecesarios, de lo contrario la prueba a ejecutar podría fallar.
+
+# Escribe tu código aquí 
+print("Bienvenido a tu curso de Universidad Python")
