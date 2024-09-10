45a16486b3997d55bea08d68672aa3ea3143e497
Pablo Daniel Magallán <82406744+pdmagallan@users.noreply.github.com>
2024-09-10 15:35:05 -0300

    Create Reto - Sistema de tienda online.py
    Variables en Python
    Crear el detalle de un producto de una tienda online y mandarlos a imprimir por consola:
    * Nombre del Producto
    * Precio del Producto
    * Cantidad en el Inventario
    * Indicar si está disponible
    Hacer algunos cambios y mandar a imprimir nuevamente el valor de las variables.

---
+++ Reto - Sistema de tienda online.py
@@ -0,0 +1,25 @@
+print('*** Sistema de Tienda Online ***')
+
+# Definir las variables de un producto
+nombre_producto = 'Cámara digital'
+precio_producto = 399.99
+cantidad_inventario = 20
+disponible_entrega = True
+
+# Mostrar información del producto
+print('Producto:', nombre_producto)
+print('Precio: $', precio_producto)
+print('Cantidad en el inventario:', cantidad_inventario)
+print('Disponible para entrega:', disponible_entrega)
+
+# Hacemos algunos cambios
+precio_producto = 299.99
+cantidad_inventario = 10
+disponible_entrega = True
+
+# Mostrar información del producto
+print()
+print('Producto:', nombre_producto)
+print('Precio: $', precio_producto)
+print('Cantidad en el inventario:', cantidad_inventario)
+print('Disponible para entrega:', disponible_entrega)
