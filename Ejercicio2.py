c39f8f3e47b001ee9d2f874a91d3b61578326671
Pablo Daniel Magallán <82406744+pdmagallan@users.noreply.github.com>
2024-09-10 15:12:52 -0300

    Create Ejercicio2.py
    Creación de Variables en Python
    Crea dos variables, la primera se llama nombre con el valor de 'Juan Perez' y la segunda variable se llama edad con el valor numérico de 30. Después manda a imprimir cada variable a consola en líneas separadas usando Python:
    
    El resultado de la salida del programa debe ser el siguiente:
    
    Juan Perez
    30
    
    Nota: La salida del programa debe ser idéntico al mostrado, respetando acentos, no agregar espacios, textos o saltos de línea innecesarios, solo los mostrados, de lo contrario la prueba a ejecutar podría fallar.

---
+++ Ejercicio2.py
@@ -0,0 +1,8 @@
+# Creacion de la variable nombre con su valor
+nombre = "Juan Perez"
+# Creacion de la variable edad con su valor
+edad = 30
+# Impresion del valor de la variable nombre
+print(nombre)
+# Impresion del valor de la variable edad
+print(edad)
