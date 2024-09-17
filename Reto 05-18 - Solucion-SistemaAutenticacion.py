bb5fee7c04e2133c8c4933b427379459cf864619
Pablo Daniel Magallán <82406744+pdmagallan@users.noreply.github.com>
2024-09-17 16:03:27 -0300

    Reto 05-18 - Solucion-SistemaAutenticacion.py

--- Reto 05-18 - Solucion-SistemaAutenticacion.py
+++ Reto 05-18 - Solucion-SistemaAutenticacion.py
@@ -1,3 +1,12 @@
+'''
+Operadores en Python
+Sistema de Autenticación
+Crea un programa para validar el usuario y password proporcionados por el usuario.
+Crea 2 constantes con los valores correctos y posteriormente compara que el usuario y password proporcionados por el usuario sean válidos.
+Debe solicitar el usuario y el password al usuario y si son iguales que los valores correctos almacenados en las constantes debe imprimir True, de lo contrario debe mandar a imprimir False.
+'''
+
+#Escribe tu codigo aqui:
 print('*** Sistema de Autenticación ***')
 USUARIO_VALIDO = 'admin'
 PASSWORD_VALIDO = '123'
